ea92aaeb1f3bf5cf3bed99a173d6c5769d23c7e1
Yui-Iida <yxuxi328@icloud.com>
2022-05-04 09:27:27 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+// const baseUrl =
+//   'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=';
+// const apiKey = '<your_api_key>&units=imperial';
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+const { json } = require('express/lib/response');
+// const req = require('express/lib/request');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3000;
+const listening = () => {
+  console.log(`running on localhost: ${port} `);
+};
+const server = app.listen(port, listening);
+console.log(server);
+
+// GET route
+// app.get('/', function (req, res) {
+//   res.send('get!');
+// });
+
+// POST route
+// app.post('/', function (req, res) {
+//   res.send('post!');
+// });
+
+// POST implementation
+/////////////////////////////////
+
+app.get('/weather', async (req, res) => {
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=`;
+  const apiKey = 'cfd0cc9081ac7bf49eec60c019850b0f';
+  const fetchResponse = await fetch(baseUrl + apiKey);
+  //     , {
+  //   method: 'GET',
+  //   credentials: 'same-origin',
+
+  //   headers: {
+  //     'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+  //     'x-rapidapi-key': apiKey,
+  //   },
+  //   body: JSON.stringify(res),
+  // }
+
+  const json = await fetchResponse.json();
+  response.json(json);
+});
---
+++ website/app.js
@@ -0,0 +1,82 @@
+'use strict';
+/* Global Variables */
+let zipCode = document.querySelector('#zip');
+let feelingToday = document.querySelector('.myInput');
+
+const date = document.querySelector('#date');
+const tempreture = document.querySelector('#temp');
+const content = document.querySelector('#content');
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+// console.log(newDate);
+
+// get
+// app.get('/', function (req, res) {
+//   res.status(200).send('Hello world!');
+// });
+
+// app.listen(3000);
+
+// const generate = () => {
+
+const generateBtn = document
+  .querySelector('#generate')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+    // console.log(zipCode.value);
+    // console.log(feelingToday.value);
+
+    date.innerHTML = newDate;
+    content.innerHTML = feelingToday.value;
+
+    // zipcode to latlon
+    const getLatLon = async zipCode => {
+      const googleMapUrl = await fetch(
+        `http://maps.googleapis.com/maps/api/geocode/json?address=santa+cruz&components=postal_code:"+${zipCode}`
+      );
+      const latlon = await googleMapUrl.json();
+      console.log(latlon);
+    };
+
+    getLatLon(zipCode);
+
+    // getWeather
+    const callApi = async (lat, lon) => {
+      // try {
+      const baseUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=`;
+      const apiKey = 'cfd0cc9081ac7bf49eec60c019850b0f';
+
+      const res = await fetch(baseUrl + apiKey);
+      const data = await res.json();
+      console.log(data);
+      tempreture.innerHTML =
+        Math.floor((data.main.temp - 273.15) * 10) / 10 + '&#8451';
+    };
+    callApi(36.2048, 138.2529);
+  });
+
+//////////////////////////////
+// let lat = 36.2048;
+// let lon = 138.2529;
+
+//     const res = await fetch(baseUrl + apiKey, {
+//       method: 'GET',
+//       credentials: 'same-origin',
+
+//       headers: {
+//         'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+//         'x-rapidapi-key': apiKey,
+//       },
+//       body: JSON.stringify(),
+//     });
+
+//     const result = await res.json();
+//     console.log(result.weather);
+//   } catch (error) {
+//     console.log('error', error);
+//   }
+// };
+
+// callApi(36.2048, 138.2529);
