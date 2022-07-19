bff2895f916336dbad9ad27a186862e19be1c2bc
Yui-Iida <yxuxi328@icloud.com>
2022-07-19 13:38:25 +0200

    update randomImg function

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,16 @@
+const fetch = require('node-fetch');
+
 // Require Express to run server and routes
-const exp = require('constants');
+// const exp = require('constants');
 const express = require('express');
 // Start up an instance of app
 const app = express();
+app.use(cors());
 
 // app.get('../client/views/index.html', function (req, res, next) {
 //   res.sendFile(__dirname + '../client/views/index.html');
 // });
+
 // Setup Server
 const port = 8080;
 app.listen(8080, console.log(`running on localhost: ${port} `));
@@ -15,7 +19,19 @@ app.listen(8080, console.log(`running on localhost: ${port} `));
 app.use(express.static('dist'));
 app.use(express.json());
 
-const geonames = require('geonames');
+var geonames = require('geonames-stream'),
+  request = require('request'),
+  through = require('through2');
+
+request
+  .get('http://download.geonames.org/export/dump/NZ.zip')
+  .pipe(geonames.pipeline)
+  .pipe(
+    through.obj(function (data, enc, next) {
+      console.log(data._id, data.name, data.population);
+      next();
+    })
+  );
 
 ////以下project3 のコード/////////////////////////////////////////////
 
@@ -23,7 +39,8 @@ const geonames = require('geonames');
 // let projectData = {};
 
 // // // Configure dotenv package
-// // require('dotenv').config();
+
+require('dotenv').config();
 // // const apiKey = `${process.env.API_KEY}`;
 
 // /* Middleware*/
@@ -50,3 +67,12 @@ app.get('/', (req, res) => {
 //   console.log(projectData);
 //   res.send(projectData);
 // });
+
+app.post('/add', (req, res) => {
+  let inputData = {
+    input: req.body,
+  };
+
+  console.log(`User destination input: ${req.destination.value}`);
+  const geonamesAPI = `http://api.geonames.org/searchJSON?name=${req.body.destination.value}&maxRows=1&username=${process.env.GEONAMES_USER}`;
+});
